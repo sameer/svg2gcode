5f197468d7c7f42176d1cab360c56a1a5c490fff
sameer <sameer@users.noreply.github.com>
2021-08-23 04:29:38 +0000

    deploy: 5206b53956ae846a9a19570893dd6ac0f6832036

--- index-130e18fb1bd191a8.js
+++ index-130e18fb1bd191a8.js
@@ -276,7 +276,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-ef06b2f0b00b4c23_bg.wasm', import.meta.url);
+        input = new URL('index-130e18fb1bd191a8_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
