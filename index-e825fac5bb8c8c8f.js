2850f5f5294a819c9568d57342d8731bd2a3a84e
sameer <sameer@users.noreply.github.com>
2021-10-03 22:24:48 +0000

    deploy: f55ab958eb9b66c4e5c5a393b827342659ff40fb

--- index-e825fac5bb8c8c8f.js
+++ index-e825fac5bb8c8c8f.js
@@ -276,7 +276,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-98c49472881b4b04_bg.wasm', import.meta.url);
+        input = new URL('index-e825fac5bb8c8c8f_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
