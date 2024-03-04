0274c2512191465658345284ef54830292b7083d
Sameer Puri <sameer@users.noreply.github.com>
2024-03-04 09:05:49 -0800

    Update dependencies and publish

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -30,14 +30,19 @@ pub struct Settings {
 #[cfg(test)]
 mod test {
     use super::*;
-    use g_code::emit::{format_gcode_fmt, FormatOptions};
+    use g_code::emit::Token;
+    use pretty_assertions::assert_eq;
     use svgtypes::{Length, LengthUnit};
+
+    /// The values change between debug and release builds for circular interpolation,
+    /// so only check within a rough tolerance
+    const TOLERANCE: f64 = 1E-10;
 
     fn get_actual(
         input: &str,
         circular_interpolation: bool,
         dimensions: [Option<Length>; 2],
-    ) -> String {
+    ) -> Vec<Token<'_>> {
         let config = ConversionConfig::default();
         let options = ConversionOptions { dimensions };
         let document = roxmltree::Document::parse(input).unwrap();
@@ -51,19 +56,41 @@ mod test {
             None,
             None,
         );
-        let program = converter::svg2program(&document, &config, options, machine);
+        converter::svg2program(&document, &config, options, machine)
+    }
 
-        let mut acc = String::new();
-        format_gcode_fmt(&program, FormatOptions::default(), &mut acc).unwrap();
-        acc
+    fn assert_close(left: Vec<Token<'_>>, right: Vec<Token<'_>>) {
+        assert_eq!(left.len(), right.len());
+        for (i, pair) in left.into_iter().zip(right.into_iter()).enumerate() {
+            match pair {
+                (Token::Field(l), Token::Field(r)) => {
+                    assert_eq!(l.letters, r.letters);
+                    if let (Some(l_value), Some(r_value)) = (l.value.as_f64(), r.value.as_f64()) {
+                        assert!(
+                            (l_value - r_value).abs() < TOLERANCE,
+                            "Values differ significantly at {i}: {l} vs {r} ({})",
+                            (l_value - r_value).abs()
+                        );
+                    } else {
+                        assert_eq!(l, r);
+                    }
+                }
+                (l, r) => {
+                    assert_eq!(l, r, "Differs at {i}");
+                }
+            }
+        }
     }
 
     #[test]
     fn square_produces_expected_gcode() {
-        let square = include_str!("../tests/square.svg");
-        let actual = get_actual(square, false, [None; 2]);
+        let expected = g_code::parse::file_parser(include_str!("../tests/square.gcode"))
+            .unwrap()
+            .iter_emit_tokens()
+            .collect::<Vec<_>>();
+        let actual = get_actual(include_str!("../tests/square.svg"), false, [None; 2]);
 
-        assert_eq!(actual, include_str!("../tests/square.gcode"))
+        assert_close(actual, expected);
     }
 
     #[test]
@@ -73,21 +100,26 @@ mod test {
             unit: LengthUnit::Mm,
         };
 
+        let expected = g_code::parse::file_parser(include_str!("../tests/square.gcode"))
+            .unwrap()
+            .iter_emit_tokens()
+            .collect::<Vec<_>>();
+
         for square in [
             include_str!("../tests/square.svg"),
             include_str!("../tests/square_dimensionless.svg"),
         ] {
-            assert_eq!(
+            assert_close(
                 get_actual(square, false, [Some(side_length); 2]),
-                include_str!("../tests/square.gcode")
+                expected.clone(),
             );
-            assert_eq!(
+            assert_close(
                 get_actual(square, false, [Some(side_length), None]),
-                include_str!("../tests/square.gcode")
+                expected.clone(),
             );
-            assert_eq!(
+            assert_close(
                 get_actual(square, false, [None, Some(side_length)]),
-                include_str!("../tests/square.gcode")
+                expected.clone(),
             );
         }
     }
@@ -95,41 +127,61 @@ mod test {
     #[test]
     fn square_transformed_produces_expected_gcode() {
         let square_transformed = include_str!("../tests/square_transformed.svg");
+        let expected =
+            g_code::parse::file_parser(include_str!("../tests/square_transformed.gcode"))
+                .unwrap()
+                .iter_emit_tokens()
+                .collect::<Vec<_>>();
         let actual = get_actual(square_transformed, false, [None; 2]);
 
-        assert_eq!(actual, include_str!("../tests/square_transformed.gcode"))
+        assert_close(actual, expected)
     }
 
     #[test]
     fn square_viewport_produces_expected_gcode() {
-        let square_transformed = include_str!("../tests/square_viewport.svg");
-        let actual = get_actual(square_transformed, false, [None; 2]);
+        let square_viewport = include_str!("../tests/square_viewport.svg");
+        let expected = g_code::parse::file_parser(include_str!("../tests/square_viewport.gcode"))
+            .unwrap()
+            .iter_emit_tokens()
+            .collect::<Vec<_>>();
+        let actual = get_actual(square_viewport, false, [None; 2]);
 
-        assert_eq!(actual, include_str!("../tests/square_viewport.gcode"))
+        assert_close(actual, expected);
     }
 
     #[test]
     fn circular_interpolation_produces_expected_gcode() {
         let circular_interpolation = include_str!("../tests/circular_interpolation.svg");
+        let expected =
+            g_code::parse::file_parser(include_str!("../tests/circular_interpolation.gcode"))
+                .unwrap()
+                .iter_emit_tokens()
+                .collect::<Vec<_>>();
         let actual = get_actual(circular_interpolation, true, [None; 2]);
 
-        assert_eq!(
-            actual,
-            include_str!("../tests/circular_interpolation.gcode")
-        )
+        assert_close(actual, expected)
     }
 
     #[test]
     fn svg_with_smooth_curves_produces_expected_gcode() {
         let svg = include_str!("../tests/smooth_curves.svg");
-        assert_eq!(
-            get_actual(svg, false, [None; 2]),
-            include_str!("../tests/smooth_curves.gcode")
-        );
 
-        assert_eq!(
+        let expected = g_code::parse::file_parser(include_str!("../tests/smooth_curves.gcode"))
+            .unwrap()
+            .iter_emit_tokens()
+            .collect::<Vec<_>>();
+
+        let expected_circular_interpolation = g_code::parse::file_parser(include_str!(
+            "../tests/smooth_curves_circular_interpolation.gcode"
+        ))
+        .unwrap()
+        .iter_emit_tokens()
+        .collect::<Vec<_>>();
+        assert_close(get_actual(svg, false, [None; 2]), expected);
+
+        assert_close(
             get_actual(svg, true, [None; 2]),
-            include_str!("../tests/smooth_curves_circular_interpolation.gcode")
+            expected_circular_interpolation,
         );
     }
 
