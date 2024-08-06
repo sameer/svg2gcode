sameer/svg2gcode

commit 8f9db323c5753f3ee88b6041becb1ee25ac7ba00

svg2gcode v0.2.0: coordinate system overhaul
This commit completely changes how the coodinate system works based on a
read-through of the W3C SVG docs. It should be _correct_ now.

Circular interpolation is still EXPERIMENTAL! There are known bugs, even
in the test output :(

Miscellaneous brain dump of changes:

- Support polygon
- Better docs
- Explained why transforms are in the opposite order of what you
  would expect
- Support preserveAspectRatio
- Proper viewBox support
- Fix SVG -> g-code coordinate system mapping
- DPI conversion: only apply after svg is processed via a wrapper turtle
- Proper handling for percentage units
- Clean up converter/mod.rs, split it up
- Nested viewport support (untested)

lib/Cargo.toml
@@ -1,6 +1,6 @@
 [package]
 name = "svg2gcode"
-version = "0.1.6"
+version = "0.2.0"
 authors = ["Sameer Puri <crates@purisa.me>"]
 edition = "2021"
 description = "Convert paths in SVG files to GCode for a pen plotter, laser engraver, or other machine."

lib/src/converter/mod.rs
@@ -1,30 +1,20 @@
 use std::fmt::Debug;
-use std::str::FromStr;
 
 use g_code::emit::Token;
-use log::{debug, warn};
-use lyon_geom::{
-    euclid::{default::Transform2D, Angle, Transform3D},
-    point, vector, ArcFlags,
-};
+use lyon_geom::euclid::default::Transform2D;
 use roxmltree::{Document, Node};
 #[cfg(feature = "serde")]
 use serde::{Deserialize, Serialize};
-use svgtypes::{
-    Length, LengthListParser, PathParser, PathSegment, PointsParser, TransformListParser,
-    TransformListToken, ViewBox,
-};
+use svgtypes::Length;
 
 use crate::{turtle::*, Machine};
 
 #[cfg(feature = "serde")]
 mod length_serde;
+mod path;
+mod transform;
+mod units;
 mod visit;
-
-const SVG_TAG_NAME: &str = "svg";
-const CLIP_PATH_TAG_NAME: &str = "clipPath";
-const PATH_TAG_NAME: &str = "path";
-const POLYLINE_TAG_NAME: &str = "polyline";
 
 /// High-level output configuration
 #[derive(Debug, Clone, PartialEq)]
@@ -69,232 +59,98 @@ pub struct ConversionOptions {
     pub dimensions: [Option<Length>; 2],
 }
 
+/// Maps SVG [`Node`]s and their attributes into operations on a [`Terrarium`]
 #[derive(Debug)]
 struct ConversionVisitor<'a, T: Turtle> {
     terrarium: Terrarium<T>,
     name_stack: Vec<String>,
-    config: &'a ConversionConfig,
+    /// Used to convert percentage values
+    viewport_dim_stack: Vec<[f64; 2]>,
+    _config: &'a ConversionConfig,
     options: ConversionOptions,
 }
 
-impl<'a, 'input: 'a> ConversionVisitor<'a, GCodeTurtle<'input>> {
-    fn begin(&mut self) {
-        // Part 1 of converting from SVG to g-code coordinates
-        self.terrarium.push_transform(Transform2D::scale(1., -1.));
+impl<'a, T: Turtle> ConversionVisitor<'a, T> {
+    fn comment(&mut self, node: &Node) {
+        let mut comment = String::new();
+        self.name_stack.iter().for_each(|name| {
+            comment += name;
+            comment += " > ";
+        });
+        comment += &node_name(node);
 
+        self.terrarium.turtle.comment(comment);
+    }
+
+    fn begin(&mut self) {
+        // Part 1 of converting from SVG to GCode coordinates
+        self.terrarium.push_transform(Transform2D::scale(1., -1.));
         self.terrarium.turtle.begin();
     }
 
     fn end(&mut self) {
-        self.terrarium.pop_transform();
         self.terrarium.turtle.end();
-    }
-}
-
-impl<'a, 'input: 'a> ConversionVisitor<'a, PreprocessTurtle> {
-    fn begin(&mut self) {
-        // Part 1 of converting from SVG to g-code coordinates
-        self.terrarium.push_transform(Transform2D::scale(1., -1.));
-    }
-
-    fn end(&mut self) {
         self.terrarium.pop_transform();
     }
 }
 
-impl<'a, T: Turtle> visit::XmlVisitor for ConversionVisitor<'a, T> {
-    fn visit_enter(&mut self, node: Node) {
-        if node.tag_name().name() == CLIP_PATH_TAG_NAME {
-            warn!("Clip paths are not supported: {:?}", node);
-        }
-
-        let mut transforms = vec![];
-
-        let view_box = node
-            .attribute("viewBox")
-            .map(ViewBox::from_str)
-            .transpose()
-            .expect("could not parse viewBox");
-        let scale_w = view_box.map(|view_box| view_box.w);
-        let scale_h = view_box.map(|view_box| view_box.h);
-        let dimensions = (
-            node.attribute("width")
-                .map(LengthListParser::from)
-                .and_then(|mut parser| parser.next())
-                .transpose()
-                .expect("could not parse width")
-                .map(|width| length_to_mm(width, self.config.dpi, scale_w)),
-            node.attribute("height")
-                .map(LengthListParser::from)
-                .and_then(|mut parser| parser.next())
-                .transpose()
-                .expect("could not parse height")
-                .map(|height| length_to_mm(height, self.config.dpi, scale_h)),
-        );
-        let aspect_ratio = match (view_box, dimensions) {
-            (_, (Some(ref width), Some(ref height))) => *width / *height,
-            (Some(ref view_box), _) => view_box.w / view_box.h,
-            (None, (None, _)) | (None, (_, None)) => 1.,
-        };
-
-        if let Some(ref view_box) = view_box {
-            let view_box_transform = Transform2D::translation(-view_box.x, -view_box.y)
-                .then_scale(1. / view_box.w, 1. / view_box.h);
-            if node.has_tag_name(SVG_TAG_NAME) {
-                // Part 2 of converting from SVG to g-code coordinates
-                transforms.push(view_box_transform.then_translate(vector(0., -1.)));
-            } else {
-                transforms.push(view_box_transform);
-            }
-        }
-
-        let dimensions_override = [
-            self.options.dimensions[0].map(|dim_x| length_to_mm(dim_x, self.config.dpi, scale_w)),
-            self.options.dimensions[1].map(|dim_y| length_to_mm(dim_y, self.config.dpi, scale_h)),
-        ];
-
-        match (dimensions_override, dimensions) {
-            ([Some(dim_x), Some(dim_y)], _) if node.has_tag_name(SVG_TAG_NAME) => {
-                transforms.push(Transform2D::scale(dim_x, dim_y));
-            }
-            ([Some(dim_x), None], _) if node.has_tag_name(SVG_TAG_NAME) => {
-                transforms.push(Transform2D::scale(dim_x, dim_x / aspect_ratio));
-            }
-            ([None, Some(dim_y)], _) if node.has_tag_name(SVG_TAG_NAME) => {
-                transforms.push(Transform2D::scale(aspect_ratio * dim_y, dim_y));
-            }
-            (_, (Some(width), Some(height))) => {
-                transforms.push(Transform2D::scale(width, height));
-            }
-            (_, (Some(width), None)) => {
-                transforms.push(Transform2D::scale(width, width / aspect_ratio));
-            }
-            (_, (None, Some(height))) => {
-                transforms.push(Transform2D::scale(aspect_ratio * height, height));
-            }
-            (_, (None, None)) => {
-                if let (Some(ViewBox { w, h, .. }), true) =
-                    (view_box, node.has_tag_name(SVG_TAG_NAME))
-                {
-                    transforms.push(Transform2D::scale(w, h));
-                    warn!("This SVG does not have width and/or height attributes! Assuming viewBox units are in millimeters");
-                }
-            }
-        }
-
-        if let Some(transform) = node.attribute("transform") {
-            let parser = TransformListParser::from(transform);
-            transforms.extend(
-                parser
-                    .map(|token| {
-                        token.expect("could not parse a transform in a list of transforms")
-                    })
-                    .map(svg_transform_into_euclid_transform)
-                    .collect::<Vec<_>>()
-                    .iter()
-                    .rev(),
-            )
-        }
-
-        self.terrarium.push_transform(
-            transforms
-                .iter()
-                .fold(Transform2D::identity(), |acc, t| acc.then(t)),
-        );
-
-        if node.tag_name().name() == PATH_TAG_NAME {
-            if let Some(d) = node.attribute("d") {
-                self.terrarium.reset();
-                let mut comment = String::new();
-                self.name_stack.iter().for_each(|name| {
-                    comment += name;
-                    comment += " > ";
-                });
-                comment += &node_name(&node);
-                self.terrarium.turtle.comment(comment);
-                apply_path(&mut self.terrarium, d);
-            } else {
-                warn!("There is a path node containing no actual path: {:?}", node);
-            }
-        } else if node.tag_name().name() == POLYLINE_TAG_NAME {
-            if let Some(points) = node.attribute("points") {
-                self.terrarium.reset();
-                let mut comment = String::new();
-                self.name_stack.iter().for_each(|name| {
-                    comment += name;
-                    comment += " > ";
-                });
-                comment += &node_name(&node);
-                self.terrarium.turtle.comment(comment);
-
-                let mut pp = PointsParser::from(points);
-
-                if let Some((x, y)) = pp.next() {
-                    self.terrarium.move_to(true, x, y);
-                }
-                while let Some((x, y)) = pp.next() {
-                    self.terrarium.line(true, x, y);
-                }
-            } else {
-                warn!(
-                    "There is a polyline node containing no actual path: {:?}",
-                    node
-                );
-            }
-        }
-
-        self.name_stack.push(node_name(&node));
-    }
-
-    fn visit_exit(&mut self, _node: Node) {
-        self.terrarium.pop_transform();
-        self.name_stack.pop();
-    }
-}
-
+/// Top-level function for converting an SVG [`Document`] into g-code
 pub fn svg2program<'a, 'input: 'a>(
     doc: &'a Document,
     config: &ConversionConfig,
     options: ConversionOptions,
     machine: Machine<'input>,
 ) -> Vec<Token<'input>> {
-    let bounding_box = {
+    let bounding_box_generator = || {
         let mut visitor = ConversionVisitor {
-            terrarium: Terrarium::new(PreprocessTurtle::default()),
-            config,
+            terrarium: Terrarium::new(DpiConvertingTurtle {
+                inner: PreprocessTurtle::default(),
+                dpi: config.dpi,
+            }),
+            _config: config,
             options: options.clone(),
             name_stack: vec![],
+            viewport_dim_stack: vec![],
         };
 
         visitor.begin();
         visit::depth_first_visit(doc, &mut visitor);
         visitor.end();
 
-        visitor.terrarium.turtle.bounding_box
+        visitor.terrarium.turtle.inner.bounding_box
     };
-
-    let origin_transform = {
-        let mut transform = Transform2D::identity();
-        if let Some(origin_x) = config.origin[0] {
-            transform = transform.then_translate(vector(origin_x - bounding_box.min.x, 0.));
+    let origin_transform = match config.origin {
+        [None, Some(origin_y)] => {
+            let bb = bounding_box_generator();
+            Transform2D::translation(0., origin_y - bb.min.y)
         }
-        if let Some(origin_y) = config.origin[1] {
-            transform = transform.then_translate(vector(0., origin_y - bounding_box.min.y));
+        [Some(origin_x), None] => {
+            let bb = bounding_box_generator();
+            Transform2D::translation(origin_x - bb.min.x, 0.)
         }
-        transform
+        [Some(origin_x), Some(origin_y)] => {
+            let bb = bounding_box_generator();
+            Transform2D::translation(origin_x - bb.min.x, origin_y - bb.min.y)
+        }
+        [None, None] => Transform2D::identity(),
     };
 
     let mut conversion_visitor = ConversionVisitor {
-        terrarium: Terrarium::new(GCodeTurtle {
-            machine,
-            tolerance: config.tolerance,
-            feedrate: config.feedrate,
-            program: vec![],
+        terrarium: Terrarium::new(DpiConvertingTurtle {
+            inner: GCodeTurtle {
+                machine,
+                tolerance: config.tolerance,
+                feedrate: config.feedrate,
+                program: vec![],
+            },
+            dpi: config.dpi,
         }),
-        config,
+        _config: config,
         options,
         name_stack: vec![],
+        viewport_dim_stack: vec![],
     };
+
     conversion_visitor
         .terrarium
         .push_transform(origin_transform);
@@ -303,7 +159,7 @@ pub fn svg2program<'a, 'input: 'a>(
     conversion_visitor.end();
     conversion_visitor.terrarium.pop_transform();
 
-    conversion_visitor.terrarium.turtle.program
+    conversion_visitor.terrarium.turtle.inner.program
 }
 
 fn node_name(node: &Node) -> String {
@@ -313,119 +169,6 @@ fn node_name(node: &Node) -> String {
         name += id;
     }
     name
-}
-
-fn apply_path<T: Turtle + Debug>(terrarium: &mut Terrarium<T>, path: &str) {
-    use PathSegment::*;
-    PathParser::from(path)
-        .map(|segment| segment.expect("could not parse path segment"))
-        .for_each(|segment| {
-            debug!("Drawing {:?}", &segment);
-            match segment {
-                MoveTo { abs, x, y } => terrarium.move_to(abs, x, y),
-                ClosePath { abs: _ } => {
-                    // Ignore abs, should have identical effect: [9.3.4. The "closepath" command]("https://www.w3.org/TR/SVG/paths.html#PathDataClosePathCommand)
-                    terrarium.close()
-                }
-                LineTo { abs, x, y } => terrarium.line(abs, x, y),
-                HorizontalLineTo { abs, x } => terrarium.line(abs, x, None),
-                VerticalLineTo { abs, y } => terrarium.line(abs, None, y),
-                CurveTo {
-                    abs,
-                    x1,
-                    y1,
-                    x2,
-                    y2,
-                    x,
-                    y,
-                } => terrarium.cubic_bezier(abs, point(x1, y1), point(x2, y2), point(x, y)),
-                SmoothCurveTo { abs, x2, y2, x, y } => {
-                    terrarium.smooth_cubic_bezier(abs, point(x2, y2), point(x, y))
-                }
-                Quadratic { abs, x1, y1, x, y } => {
-                    terrarium.quadratic_bezier(abs, point(x1, y1), point(x, y))
-                }
-                SmoothQuadratic { abs, x, y } => {
-                    terrarium.smooth_quadratic_bezier(abs, point(x, y))
-                }
-                EllipticalArc {
-                    abs,
-                    rx,
-                    ry,
-                    x_axis_rotation,
-                    large_arc,
-                    sweep,
-                    x,
-                    y,
-                } => terrarium.elliptical(
-                    abs,
-                    vector(rx, ry),
-                    Angle::degrees(x_axis_rotation),
-                    ArcFlags { large_arc, sweep },
-                    point(x, y),
-                ),
-            }
-        });
-}
-
-fn svg_transform_into_euclid_transform(svg_transform: TransformListToken) -> Transform2D<f64> {
-    use TransformListToken::*;
-    match svg_transform {
-        Matrix { a, b, c, d, e, f } => Transform2D::new(a, b, c, d, e, f),
-        Translate { tx, ty } => Transform2D::translation(tx, ty),
-        Scale { sx, sy } => Transform2D::scale(sx, sy),
-        Rotate { angle } => Transform2D::rotation(Angle::degrees(angle)),
-        // https://drafts.csswg.org/css-transforms/#SkewXDefined
-        SkewX { angle } => Transform3D::skew(Angle::degrees(angle), Angle::zero()).to_2d(),
-        // https://drafts.csswg.org/css-transforms/#SkewYDefined
-        SkewY { angle } => Transform3D::skew(Angle::zero(), Angle::degrees(angle)).to_2d(),
-    }
-}
-
-/// Convenience function for converting absolute lengths to millimeters
-///
-/// Absolute lengths are listed in [CSS 4 §6.2](https://www.w3.org/TR/css-values/#absolute-lengths).
-/// Relative lengths in [CSS 4 §6.1](https://www.w3.org/TR/css-values/#relative-lengths) are not supported and will simply be interpreted as millimeters.
-///
-/// A default DPI of 96 is used as per [CSS 4 §7.4](https://www.w3.org/TR/css-values/#resolution), which you can adjust with --dpi.
-/// Increasing DPI reduces the scale of an SVG.
-fn length_to_mm(l: svgtypes::Length, dpi: f64, scale: Option<f64>) -> f64 {
-    const DEFAULT_SVG_DPI: f64 = 96.;
-    use svgtypes::LengthUnit::*;
-    use uom::si::f64::Length;
-    use uom::si::length::*;
-
-    let dpi_scaling = dpi / DEFAULT_SVG_DPI;
-    let length = match l.unit {
-        Cm => Length::new::<centimeter>(l.number),
-        Mm => Length::new::<millimeter>(l.number),
-        In => Length::new::<inch>(l.number),
-        Pc => Length::new::<pica_computer>(l.number) / dpi_scaling,
-        Pt => Length::new::<point_computer>(l.number) / dpi_scaling,
-        Px => Length::new::<inch>(l.number / dpi),
-        Em => {
-            warn!("Converting from em to millimeters assumes 1em = 16px");
-            Length::new::<inch>(16. * l.number / dpi)
-        }
-        Percent => {
-            if let Some(scale) = scale {
-                warn!("Converting from percent to millimeters assumes the viewBox is specified in millimeters");
-                Length::new::<millimeter>(l.number / 100. * scale)
-            } else {
-                warn!("Converting from percent to millimeters without a viewBox is not possible, treating as millimeters");
-                Length::new::<millimeter>(l.number)
-            }
-        }
-        other => {
-            warn!(
-                "Converting from '{:?}' to millimeters is not supported, treating as millimeters",
-                other
-            );
-            Length::new::<millimeter>(l.number)
-        }
-    };
-
-    length.get::<millimeter>()
 }
 
 #[cfg(test)]

lib/src/converter/units.rs
@@ -1,0 +1,82 @@
+use log::warn;
+use roxmltree::Node;
+use svgtypes::{Length, LengthListParser};
+
+use crate::Turtle;
+
+use super::ConversionVisitor;
+
+/// Used to compute percentages correctly
+///
+/// <https://www.w3.org/TR/SVG/coords.html#Units>
+#[derive(Clone, Copy)]
+pub enum DimensionHint {
+    Horizontal,
+    Vertical,
+    Other,
+}
+
+impl<'a, T: Turtle> ConversionVisitor<'a, T> {
+    /// Convenience function for converting a length attribute to user units
+    pub fn length_attr_to_user_units(&self, node: &Node, attr: &str) -> Option<f64> {
+        let l = node
+            .attribute(attr)
+            .map(LengthListParser::from)
+            .and_then(|mut parser| parser.next())
+            .transpose()
+            .ok()
+            .flatten()?;
+
+        Some(self.length_to_user_units(
+            l,
+            match attr {
+                "x" | "x1" | "x2" | "cx" | "rx" | "width" => DimensionHint::Horizontal,
+                "y" | "y1" | "y2" | "cy" | "ry" | "height" => DimensionHint::Vertical,
+                _ => DimensionHint::Other,
+            },
+        ))
+    }
+    /// Convenience function for converting [`Length`] to user units
+    ///
+    /// Absolute lengths are listed in [CSS 4 §6.2](https://www.w3.org/TR/css-values/#absolute-lengths).
+    /// Relative lengths in [CSS 4 §6.1](https://www.w3.org/TR/css-values/#relative-lengths) are not supported and will simply be interpreted as millimeters.
+    ///
+    /// A default DPI of 96 is used as per [CSS 4 §7.4](https://www.w3.org/TR/css-values/#resolution)
+    pub fn length_to_user_units(&self, l: Length, hint: DimensionHint) -> f64 {
+        use svgtypes::LengthUnit::*;
+        use uom::si::f64::Length;
+        use uom::si::length::*;
+
+        const CSS_DEFAULT_DPI: f64 = 96.;
+
+        match l.unit {
+            Cm => Length::new::<centimeter>(l.number).get::<inch>() * CSS_DEFAULT_DPI,
+            Mm => Length::new::<millimeter>(l.number).get::<inch>() * CSS_DEFAULT_DPI,
+            In => Length::new::<inch>(l.number).get::<inch>() * CSS_DEFAULT_DPI,
+            Pc => Length::new::<pica_computer>(l.number).get::<inch>() * CSS_DEFAULT_DPI,
+            Pt => Length::new::<point_computer>(l.number).get::<inch>() * CSS_DEFAULT_DPI,
+            // https://www.w3.org/TR/SVG/coords.html#ViewportSpace says None should be treated as Px
+            Px | None => l.number,
+            Em | Ex => {
+                warn!("Converting from em/ex to millimeters assumes 1em/ex = 16px");
+                16. * l.number
+            }
+            // https://www.w3.org/TR/SVG/coords.html#Units
+            Percent => {
+                if let Some([width, height]) = self.viewport_dim_stack.last() {
+                    let scale = match hint {
+                        DimensionHint::Horizontal => *width,
+                        DimensionHint::Vertical => *height,
+                        DimensionHint::Other => {
+                            (width.powi(2) + height.powi(2)).sqrt() / 2.0_f64.sqrt()
+                        }
+                    };
+                    l.number / 100. * scale
+                } else {
+                    warn!("A percentage without an established viewport is not valid!");
+                    l.number / 100.
+                }
+            }
+        }
+    }
+}

lib/src/lib.rs
@@ -3,11 +3,11 @@ use serde::{Deserialize, Serialize};
 
 /// Approximate [Bézier curves](https://en.wikipedia.org/wiki/B%C3%A9zier_curve) with [Circular arcs](https://en.wikipedia.org/wiki/Circular_arc)
 mod arc;
-/// Converts an SVG to G-Code in an internal representation
+/// Converts an SVG to an internal representation
 mod converter;
 /// Emulates the state of an arbitrary machine that can run G-Code
 mod machine;
-/// Operations that are easier to implement after G-Code is generated, or would
+/// Operations that are easier to implement while/after G-Code is generated, or would
 /// otherwise over-complicate SVG conversion
 mod postprocess;
 /// Provides an interface for drawing lines in G-Code
@@ -131,6 +131,19 @@ mod test {
         let square_transformed = include_str!("../tests/square_transformed.svg");
         let expected =
             g_code::parse::file_parser(include_str!("../tests/square_transformed.gcode"))
+                .unwrap()
+                .iter_emit_tokens()
+                .collect::<Vec<_>>();
+        let actual = get_actual(square_transformed, false, [None; 2]);
+
+        assert_close(actual, expected)
+    }
+
+    #[test]
+    fn square_transformed_nested_produces_expected_gcode() {
+        let square_transformed = include_str!("../tests/square_transformed_nested.svg");
+        let expected =
+            g_code::parse::file_parser(include_str!("../tests/square_transformed_nested.gcode"))
                 .unwrap()
                 .iter_emit_tokens()
                 .collect::<Vec<_>>();

commit e50e42b2c60ce4793457c480c92bd66abaec86ca

v0.3.1: Fix origin override broken by 8f9db323c5753f3ee88b6041becb1ee25ac7ba00
This now needs to be converted from mm into user units

cli/Cargo.toml
@@ -1,6 +1,6 @@
 [package]
 name = "svg2gcode-cli"
-version = "0.0.13"
+version = "0.0.14"
 authors = ["Sameer Puri <crates@purisa.me>"]
 edition = "2021"
 description = "Command line interface for svg2gcode"
@@ -8,7 +8,7 @@ repository = "https://github.com/sameer/svg2gcode"
 license = "MIT"
 
 [dependencies]
-svg2gcode = { version = "0.3.0", features = ["serde"] }
+svg2gcode = { version = "0.3.1", features = ["serde"] }
 env_logger = { version = "0", default-features = false, features = [
     "atty",
     "termcolor",

lib/Cargo.toml
@@ -1,6 +1,6 @@
 [package]
 name = "svg2gcode"
-version = "0.3.0"
+version = "0.3.1"
 authors = ["Sameer Puri <crates@purisa.me>"]
 edition = "2021"
 description = "Convert paths in SVG files to GCode for a pen plotter, laser engraver, or other machine."

lib/src/converter/mod.rs
@@ -6,8 +6,11 @@ use roxmltree::{Document, Node};
 #[cfg(feature = "serde")]
 use serde::{Deserialize, Serialize};
 use svgtypes::Length;
+use uom::si::f64::Length as UomLength;
+use uom::si::length::{inch, millimeter};
 
 use crate::{turtle::*, Machine};
+use self::units::CSS_DEFAULT_DPI;
 
 #[cfg(feature = "serde")]
 mod length_serde;
@@ -26,7 +29,7 @@ pub struct ConversionConfig {
     pub feedrate: f64,
     /// Dots per inch for pixels, picas, points, etc.
     pub dpi: f64,
-    /// Set the origin point for this conversion
+    /// Set the origin point in millimeters for this conversion
     #[cfg_attr(feature = "serde", serde(default = "zero_origin"))]
     pub origin: [Option<f64>; 2],
 }
@@ -119,7 +122,13 @@ pub fn svg2program<'a, 'input: 'a>(
 
         visitor.terrarium.turtle.inner.bounding_box
     };
-    let origin_transform = match config.origin {
+
+    // Convert from millimeters to user units
+    let origin = config
+        .origin
+        .map(|dim| dim.map(|d| UomLength::new::<millimeter>(d).get::<inch>() * CSS_DEFAULT_DPI));
+
+    let origin_transform = match origin {
         [None, Some(origin_y)] => {
             let bb = bounding_box_generator();
             Transform2D::translation(0., origin_y - bb.min.y)

lib/src/converter/units.rs
@@ -6,6 +6,11 @@ use crate::Turtle;
 
 use super::ConversionVisitor;
 
+/// The DPI assumed by CSS is 96.
+///
+/// <https://www.w3.org/TR/css3-values/#absolute-lengths>
+pub const CSS_DEFAULT_DPI: f64 = 96.;
+
 /// Used to compute percentages correctly
 ///
 /// <https://www.w3.org/TR/SVG/coords.html#Units>
@@ -46,8 +51,6 @@ impl<'a, T: Turtle> ConversionVisitor<'a, T> {
         use svgtypes::LengthUnit::*;
         use uom::si::f64::Length;
         use uom::si::length::*;
-
-        const CSS_DEFAULT_DPI: f64 = 96.;
 
         match l.unit {
             Cm => Length::new::<centimeter>(l.number).get::<inch>() * CSS_DEFAULT_DPI,

lib/src/lib.rs
@@ -34,7 +34,7 @@ impl Settings {
     ///
     /// - Settings version is [`Version::Unknown`].
     /// - There are breaking changes requiring manual intervention. In which case this does a partial update to that point.
-    pub fn try_upgrade(&mut self) -> Result<(), ()> {
+    pub fn try_upgrade(&mut self) -> Result<(), &'static str> {
         loop {
             match self.version {
                 // Compatibility for M2 by default
@@ -46,7 +46,7 @@ impl Settings {
                     self.version = Version::V5;
                 }
                 Version::V5 => break Ok(()),
-                Version::Unknown(_) => break Err(()),
+                Version::Unknown(_) => break Err("cannot upgrade unknown version"),
             }
         }
     }

web/Cargo.toml
@@ -1,6 +1,6 @@
 [package]
 name = "svg2gcode-web"
-version = "0.0.14"
+version = "0.0.15"
 authors = ["Sameer Puri <crates@purisa.me>"]
 edition = "2021"
 description = "Convert vector graphics to g-code for pen plotters, laser engravers, and other CNC machines"
@@ -10,7 +10,7 @@ license = "MIT"
 
 [dependencies]
 wasm-bindgen = "0.2"
-svg2gcode = { version = "0.3.0", features = ["serde"] }
+svg2gcode = { version = "0.3.1", features = ["serde"] }
 roxmltree = "0.19"
 g-code = "0.4.2"
 codespan-reporting = "0.11"