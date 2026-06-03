d8062f12ebd752723272fdefb1144114b77be496
sameer <sameer@users.noreply.github.com>
2026-06-03 00:35:38 +0000

    deploy: 81893c0a0e1f38ec1ed614d2f94f93bb78308b89

--- svg2gcode-web-34002b5092b24ee6.js
+++ svg2gcode-web-34002b5092b24ee6.js
@@ -554,7 +554,7 @@ function __wbg_get_imports() {
             console.warn(arg0, arg1, arg2, arg3);
         },
         __wbindgen_cast_0000000000000001: function(arg0, arg1) {
-            // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [Externref], shim_idx: 1489, ret: Result(Unit), inner_ret: Some(Result(Unit)) }, mutable: true }) -> Externref`.
+            // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [Externref], shim_idx: 1496, ret: Result(Unit), inner_ret: Some(Result(Unit)) }, mutable: true }) -> Externref`.
             const ret = makeMutClosure(arg0, arg1, wasm_bindgen__convert__closures_____invoke__h28afee4b70401b8d);
             return ret;
         },
